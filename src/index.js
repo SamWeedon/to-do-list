e7d4db62a4050d29bd61d5a92cc670b5e15de838
SamWeedon <64287633+SamWeedon@users.noreply.github.com>
2023-12-03 17:03:27 -0700

    Add some basic DOM loading

--- src/index.js
+++ src/index.js
@@ -14,12 +14,32 @@ const defaultToDo = ToDo('groceries', 'get milk', '12-8', 'high');
 const ToDo1 = ToDo('eat', 'eat dinner', '12-9', 'low');
 const ToDo2 = ToDo('run', '3 miles', '12-10', 'medium');
 
-//console.log(defaultToDo.title, defaultToDo.description, defaultToDo.dueDate, defaultToDo.priority);
-
 let defaultProject = [defaultToDo, ToDo1, ToDo2];
 let projectList = [defaultProject];
 
+
+function loadDOM() {
+    for (let project of projectList) {
+        for (let todo of project) {
+            const container = document.createElement('div');
+            const title = document.createElement('h2');
+            title.textContent = todo.title;
+            const description = document.createElement('p');
+            description.textContent = todo.description;
+            const dueDate = document.createElement('p');
+            dueDate.textContent = todo.dueDate;
+            document.body.appendChild(container);
+            container.appendChild(title);
+            container.appendChild(description);
+            container.appendChild(dueDate);
+        }
+    }
+}
+
+loadDOM();
+
 function removeObjectWithPropertyValue(property, value, list) {
+    // generic function for removing an object with a given property value from a list
     let index;
     for (let obj of list) {
         if (obj[property] == value) {
@@ -30,18 +50,22 @@ function removeObjectWithPropertyValue(property, value, list) {
 }
 
 function changeObjectPropertyValue(obj, property, newValue) {
+    // generic function for changing object properties
     obj[property] = newValue; 
 }
 
 function toggleBooleanProperty(obj, property) {
+    // generic function for toggling boolean object properties
     obj[property] = !obj[property];
 }
 
-//function nestListWithinList(nestedList, parentList) {
-//    parentList.unshift(nestedList);
-//}
+function nestListWithinList(nestedList, parentList) {
+    // generic function for adding something to the beginning of a list
+    parentList.unshift(nestedList);
+}
 
 function addProject(projectList, project) {
+    // specific function for adding a project to the list
     projectList.unshift(project);
 }
 
@@ -50,3 +74,4 @@ changeObjectPropertyValue(ToDo2, 'description', '10 miles');
 toggleBooleanProperty(ToDo2, 'complete');
 
 console.log(defaultProject);
+
