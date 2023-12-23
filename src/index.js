dcdc664fad9587265b70790c7cab125809b9be12
SamWeedon <64287633+SamWeedon@users.noreply.github.com>
2023-12-23 16:45:51 -0700

    Add todo minimization/maximization

--- src/index.js
+++ src/index.js
@@ -6,19 +6,21 @@
 // mark todos as complete
 // project names
 import './style.css';
+import { format } from 'date-fns';
 
 const ToDo = (title, description, dueDate, priority) => {
     const complete = false;
-    return {title, description, dueDate, priority, complete};
+    const maximized = false;
+    return {title, description, dueDate, priority, complete, maximized};
 };
 
 const Project = (title, todoList) => {
     return {title, todoList};
 }
 
-const defaultToDo = ToDo('groceries', 'get milk', new Date('2023-12-08'), 'high');
-const ToDo1 = ToDo('eat', 'eat dinner', new Date('2023-12-09'), 'low');
-const ToDo2 = ToDo('run', '3 miles', new Date('2023-12-10'), 'medium');
+const defaultToDo = ToDo('groceries', 'get milk', new Date('2023-12-08T00:00'), 'high');
+const ToDo1 = ToDo('eat', 'eat dinner', new Date('2023-12-09T00:00'), 'low');
+const ToDo2 = ToDo('run', '3 miles', new Date('2023-12-10T00:00'), 'medium');
 
 let defaultProject = Project('default', [defaultToDo, ToDo1, ToDo2]);
 let projectList = [defaultProject];
@@ -42,7 +44,7 @@ function loadDOM() {
         projectBox.appendChild(projectHeading);
 
         const addTodoButton = document.createElement('button');
-        addTodoButton.textContent = '+';
+        addTodoButton.textContent = 'Add ToDo';
         addTodoButton.addEventListener('click', function() {
             project.todoList.unshift(ToDo('','','',''));
             loadDOM();
@@ -61,75 +63,100 @@ function loadDOM() {
         for (let todo of project.todoList) {
             const todoBox = document.createElement('div');
             todoBox.classList.add('todoBox');
-
-            const completeButton = document.createElement('button');
-            completeButton.textContent = 'Complete';
-            todo.complete ? completeButton.style.backgroundColor = 'green' : completeButton.style.backgroundColor = 'initial';
-            completeButton.addEventListener('click', function() {
-                todo.complete = !todo.complete;
-                todo.complete ? completeButton.style.backgroundColor = 'green' : completeButton.style.backgroundColor = 'initial';               
-            })
-            todoBox.appendChild(completeButton);
-
-            const title = document.createElement('textarea');
-            title.textContent = todo.title;
-            title.addEventListener('input', function() {
-                todo.title = title.value;
-                console.log(todo);
-            })
-
-            const description = document.createElement('textarea');
-            description.textContent = todo.description;
-            description.addEventListener('input', function() {
-                todo.description = description.value;
-                console.log(todo);
-            })
-
-            const dueDate = document.createElement('input');
-            dueDate.type = 'date';
-            if (todo.dueDate) {
-                dueDate.valueAsDate = todo.dueDate;
-            }
-            dueDate.addEventListener('input', function() {
-                todo.dueDate = new Date(dueDate.value);
-            })
-
-            const prioritySelect = document.createElement('select');
-            const lowPriority = document.createElement('option');
-            lowPriority.value = 'low';
-            lowPriority.textContent = 'Low';
-            const mediumPriority = document.createElement('option');
-            mediumPriority.value = 'medium';
-            mediumPriority.textContent = 'Medium';
-            const highPriority = document.createElement('option');
-            highPriority.value = 'high';
-            highPriority.textContent = 'High';
-            prioritySelect.appendChild(lowPriority);
-            prioritySelect.appendChild(mediumPriority);
-            prioritySelect.appendChild(highPriority);
-
-            prioritySelect.value = todo.priority;
-            prioritySelect.addEventListener('change', function() {
-                todo.priority = prioritySelect.value;
-            })
-
-            const deleteButton = document.createElement('button');
-            deleteButton.textContent = 'delete';
-            const editButton = document.createElement('button');
-            editButton.textContent = 'edit';
-
             projectBox.appendChild(todoBox);
-            todoBox.appendChild(title);
-            todoBox.appendChild(description);
-            todoBox.appendChild(dueDate);
-            todoBox.appendChild(prioritySelect);
-            todoBox.appendChild(deleteButton);
-            
-            deleteButton.addEventListener('click', function(e) {
-                const button = e.target;
-                removeObjectWithPropertyValue('title', todo.title, project.todoList);
+
+            if (todo.maximized) {
+                const completeButton = document.createElement('button');
+                completeButton.textContent = 'Complete';
+                todo.complete ? completeButton.style.backgroundColor = 'green' : completeButton.style.backgroundColor = 'initial';
+                completeButton.addEventListener('click', function() {
+                    todo.complete = !todo.complete;
+                    todo.complete ? completeButton.style.backgroundColor = 'green' : completeButton.style.backgroundColor = 'initial';               
+                })
+                todoBox.appendChild(completeButton);
+    
+                const title = document.createElement('textarea');
+                title.textContent = todo.title;
+                title.addEventListener('input', function() {
+                    todo.title = title.value;
+                    console.log(todo);
+                })
+                todoBox.appendChild(title);
+    
+                const description = document.createElement('textarea');
+                description.textContent = todo.description;
+                description.addEventListener('input', function() {
+                    todo.description = description.value;
+                    console.log(todo);
+                })
+                todoBox.appendChild(description);
+    
+                const dueDate = document.createElement('input');
+                dueDate.type = 'date';
+                if (todo.dueDate) {
+                    dueDate.valueAsDate = todo.dueDate;
+                }
+                dueDate.addEventListener('input', function() {
+                    todo.dueDate = new Date(dueDate.value + 'T00:00');
+                })
+                todoBox.appendChild(dueDate);
+    
+                const prioritySelect = document.createElement('select');
+                const lowPriority = document.createElement('option');
+                lowPriority.value = 'low';
+                lowPriority.textContent = 'Low';
+                const mediumPriority = document.createElement('option');
+                mediumPriority.value = 'medium';
+                mediumPriority.textContent = 'Medium';
+                const highPriority = document.createElement('option');
+                highPriority.value = 'high';
+                highPriority.textContent = 'High';
+                prioritySelect.appendChild(lowPriority);
+                prioritySelect.appendChild(mediumPriority);
+                prioritySelect.appendChild(highPriority);
+    
+                prioritySelect.value = todo.priority;
+                prioritySelect.addEventListener('change', function() {
+                    todo.priority = prioritySelect.value;
+                })
+                todoBox.appendChild(prioritySelect);
+    
+                const deleteButton = document.createElement('button');
+                deleteButton.textContent = 'delete';
+                const editButton = document.createElement('button');
+                editButton.textContent = 'edit';
+                todoBox.appendChild(deleteButton);
+                
+                deleteButton.addEventListener('click', function(e) {
+                    const button = e.target;
+                    removeObjectWithPropertyValue('title', todo.title, project.todoList);
+                    loadDOM();
+                })
+            }
+
+            else {
+                const miniTitle = document.createElement('h3');
+                miniTitle.textContent = todo.title;
+                todoBox.appendChild(miniTitle);
+                
+                if (todo.dueDate) {
+                    const miniDueDate = document.createElement('h4');
+                    miniDueDate.textContent = format(todo.dueDate, 'MM/dd/yyyy');
+                    todoBox.appendChild(miniDueDate);
+                }
+            }
+
+            const editButton = document.createElement('button');
+            if (todo.maximized) editButton.textContent = 'Minimize';
+            else editButton.textContent = 'Expand';
+            editButton.addEventListener('click', function() {
+                todo.maximized = !todo.maximized;
                 loadDOM();
-            })
+            });
+            //editButton.addEventListener('click', function(e) {
+            //    e.target.classList.toggle('clicked');
+            //});
+            todoBox.appendChild(editButton);
         }
     }
 
